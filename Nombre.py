0d3c4d9657d79cfdea1416cfe4948a1d55c80c5c
JuanManuEstrada <55827939+JuanManuEstrada@users.noreply.github.com>
2019-09-26 02:18:06 -0500

    Create Nombre.py

---
+++ Nombre.py
@@ -0,0 +1,11 @@
+Nombre = input("Nombre:")
+Apellidos = input("Apellidos:")
+# Agregamos y preguntamos las variables de el nombre
+# y el apellido
+
+NombreCompleto = Nombre + " " + Apellidos
+# El nombre y el apellido se juntaran en una sola variable
+
+NombreCompleto = NombreCompleto.upper()
+print(NombreCompleto)
+# Al juntarlos en NombreCompleto se imprimiran juntos
