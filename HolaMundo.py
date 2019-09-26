615712dfc3eaa6ef9753e433cd0108c450d67d72
JuanManuEstrada <55827939+JuanManuEstrada@users.noreply.github.com>
2019-09-26 02:12:47 -0500

    Create HolaMundo.py

---
+++ HolaMundo.py
@@ -0,0 +1,7 @@
+# HolaMundo.py
+# Autor: Juan Estrada
+# Fecha de creacion: 25/09/2019
+
+print("Hola Mundo, ¡ahora en phyton")
+# Con print generamos la salida de consola
+# se va a ver lo que escribimos en los ""
